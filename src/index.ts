ce87b6740671e7bf8d2432edf5c8b50f9a51c36e
Evgeny Kolotov <e.Kolotov1995@gmail.com>
2021-05-09 13:54:29 +0300

    [EX-0002] Изменено расширение всех js файлов на ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// import Router from '@core/routes/Router';
-// import DashboardPage from './pages/DashboardPage';
-// import ExcelPage from './pages/ExcelPage';
-// import './scss/index.scss';
-//
-// new Router('#application', {
-//   excel: ExcelPage,
-//   dashboard: DashboardPage,
-// });
+import Router from '@core/routes/Router';
+import DashboardPage from './pages/DashboardPage';
+import ExcelPage from './pages/ExcelPage';
+import './scss/index.scss';
+
+new Router('#application', {
+  excel: ExcelPage,
+  dashboard: DashboardPage,
+});
